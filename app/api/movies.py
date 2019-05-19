267fd0415f7dd2de0c1ac4542dbd84f8a4c5dfec
keni7385 <andrea.corsini@outlook.com>
2019-05-20 01:05:07 +0200

    Tests API movie + bug fixes

--- app/api/movies.py
+++ app/api/movies.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, current_app, g
 from sqlalchemy import func
+from app import db
 from app.api import bp
 from app.api.auth import token_auth
 from app.models import Movie, Interaction
@@ -15,37 +16,42 @@ RECOMMENDED_PER_PAGE_MAX = 100
 
 @bp.route('/movie/toppop', methods=['GET'])
 @token_auth.login_required
-def get_top_popular(id):
+def get_top_popular():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', TOP_POPULAR_PER_PAGE_MIN, type=int), TOP_POPULAR_PER_PAGE_MAX)
-    q = Movie.query.order_by(func.cont(Movie.spectators).desc())
+    q = Movie.query\
+        .join(Interaction)\
+        .group_by(Movie.id)\
+        .order_by(func.count(Interaction.user_id).desc())
     data = Movie.to_collection_dict(q, page, per_page, 'api.get_top_popular')
     return jsonify(data)
 
 
-@bp.route('/movie/<int:id>/watched', methods=['GET'])
+@bp.route('/movie/watched/<int:id>', methods=['GET'])
 @token_auth.login_required
 def get_watched_movies(id):
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', WATCHED_PER_PAGE_MIN, type=int), WATCHED_PER_PAGE_MAX)
     user = g.current_user
     q = Movie.query\
-        .join(Interaction.query.filter(Interaction.user_id == user.id, Interaction.score == Interaction.IMPLICIT_RATE),
-              Movie.movie_id == Interaction.movie_id)
-    data = Movie.to_collection_dict(q, page, per_page, 'api.get_users')
+        .join(Interaction)\
+        .filter(Interaction.user_id == user.id, Interaction.score == Interaction.IMPLICIT_RATE)
+    data = Movie.to_collection_dict(q, page, per_page, 'api.get_watched_movies', id=user.id)
     return jsonify(data)
 
 
-@bp.route('/movie/<int:id>/recommended', methods=['GET'])
+@bp.route('/movie/recommended/<int:id>', methods=['GET'])
 @token_auth.login_required
 def get_recommended_movies(id):
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', RECOMMENDED_PER_PAGE_MIN, type=int), RECOMMENDED_PER_PAGE_MAX)
-    data = Movie.to_collection_dict(Movie.query, page, per_page, 'api.get_users')  # TODO: setup recommendation
+    user = g.current_user
+    data = Movie.to_collection_dict(Movie.query, page, per_page, 'api.get_recommended_movies', id=user.id)
+    # TODO: setup recommendation
     return jsonify(data)
 
 
 @bp.route('/movie/<int:id>', methods=['GET'])
 @token_auth.login_required
 def get_movie(id):
-    return jsonify(Movie.query().get_or_404(id).to_dict())
+    return jsonify(Movie.query.get_or_404(id, description='Movie not found').to_dict())
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ import unittest
 from flask import jsonify
 from base64 import b64encode
 from app import create_app, db
-from app.models import User
+from app.models import User, Movie
 from tests.test_config import TestConfig
 
 
@@ -68,6 +68,7 @@ class ApiCase(unittest.TestCase):
     ############
     # Token API
     ############
+
     def _fixture_get_token(self):
         u = User(id=1, username='u', firstName='f', lastName='l', email='e')
         u.set_password('p')
@@ -113,3 +114,98 @@ class ApiCase(unittest.TestCase):
         rv_get = self.client.get('/api/user/' + str(rv.get_json()['sub']),
                                  headers={'Authorization': 'Bearer %s' % rv.get_json()['token']})
         self.assertEqual(401, rv_get.status_code)
+
+    ############
+    # Movie API
+    ############
+
+    def testGetTopPop(self):
+        rv, u = self._fixture_get_token()
+
+        movies = []
+        for i in range(3):
+            movies.append(Movie(title='t%d' % i, description='d%d' % i))
+            db.session.add(movies[i])
+
+        users = []
+        for i in range(3):
+            users.append(User())
+            users[i].watch(movies[i % 2])
+            db.session.add(users[i])
+
+        db.session.commit()
+
+        expected = {
+            '_links': {'next': None, 'prev': None, 'self': '/api/movie/toppop?page=1&per_page=10'},
+            '_meta': {'page': 1, 'per_page': 10, 'total_items': 2, 'total_pages': 1},
+            'items': [
+                {'_links': {'self': 'http://localhost:5000/api/movie/1', 'video': None},
+                 'description': 'd0', 'id': 1, 'rating': {'score': 0.0, 'support': 0}, 'title': 't0'},
+                {'_links': {'self': 'http://localhost:5000/api/movie/2', 'video': None},
+                 'description': 'd1', 'id': 2, 'rating': {'score': 0.0, 'support': 0}, 'title': 't1'},
+            ]
+        }
+
+        rv_toppop = self.client.get('/api/movie/toppop',
+                                    headers={'Authorization': 'Bearer %s' % rv.get_json()['token']})
+
+        self.assertEqual(200, rv_toppop.status_code)
+        self.assertEqual(expected, rv_toppop.get_json())
+
+    def testEmptyGetWatchedMovie(self):
+        rv, u = self._fixture_get_token()
+        rv_watched = self.client.get('/api/movie/watched/%d' % u.id,
+                                     headers={'Authorization': 'Bearer %s' % rv.get_json()['token']})
+
+        expected = {
+            '_links': {'next': None, 'prev': None, 'self': '/api/movie/watched/1?page=1&per_page=10'},
+            '_meta': {'page': 1, 'per_page': 10, 'total_items': 0, 'total_pages': 0},
+            'items': []
+        }
+
+        self.assertEqual(200, rv_watched.status_code)
+        self.assertEqual(expected, rv_watched.get_json())
+
+    def testGetWatchedMovie(self):
+        rv, u = self._fixture_get_token()
+        m1 = Movie(id=1, title='t1', description='d1')
+        m2 = Movie(id=2, title='t2', description='d2')
+        u.watch(m2)
+        db.session.add(m1)
+        db.session.add(m2)
+        db.session.commit()
+        rv_watched = self.client.get('/api/movie/watched/%d' % u.id,
+                                     headers={'Authorization': 'Bearer %s' % rv.get_json()['token']})
+
+        expected = {
+            '_links': {'next': None, 'prev': None, 'self': '/api/movie/watched/1?page=1&per_page=10'},
+            '_meta': {'page': 1, 'per_page': 10, 'total_items': 1, 'total_pages': 1},
+            'items': [
+                {'_links': {'self': 'http://localhost:5000/api/movie/2', 'video': None},
+                 'description': 'd2', 'id': 2, 'rating': {'score': 0.0, 'support': 0}, 'title': 't2'},
+            ]
+        }
+
+        self.assertEqual(200, rv_watched.status_code)
+        self.assertEqual(expected, rv_watched.get_json())
+
+    def testGetMovie(self):
+        rv, u = self._fixture_get_token()
+
+        m = Movie(title='t', description='d')
+        db.session.add(m)
+        db.session.commit()
+
+        rv_watched = self.client.get('/api/movie/%d' % m.id,
+                                     headers={'Authorization': 'Bearer %s' % rv.get_json()['token']})
+
+        expected = {
+            '_links': {'self': 'http://localhost:5000/api/movie/1', 'video': None},
+            'description': 'd',
+            'id': 1,
+            'rating': {'score': 0.0, 'support': 0},
+            'title': 't'
+        }
+
+        self.assertEqual(200, rv_watched.status_code)
+        self.assertEqual(expected, rv_watched.get_json())
