7d3e9261f0ce6541b3fdd2eba1e68469be76598b
keni7385 <andrea.corsini@outlook.com>
2019-05-20 20:48:07 +0200

    Removed localhost production config

--- config.py
+++ config.py
@@ -11,5 +11,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = 'localhost:5000'
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
